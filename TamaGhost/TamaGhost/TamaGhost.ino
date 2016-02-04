55731a95cbf97ff9893eab569e17f0c48c117fa4
Alexandra Olivier <Alex@Alexandras-MacBook-Pro.local>
2016-02-04 14:11:22 -0500

    Code with transitions, need to fix gradient.

--- TamaGhost/TamaGhost/TamaGhost.ino
+++ TamaGhost/TamaGhost/TamaGhost.ino
@@ -74,10 +74,31 @@ int curY = 0;
 int y_sign = 1;
 int x_sign = 1;
 
+unsigned long prevMillis;
 
 int fadeIncrement = 2;
 int fadeCount = 255;
 bool fadeMonster = false;
+
+int currentGhostType = 0;
+float ghostTypeTimer = 0;
+int ghostTypeInterval = 30 * 1000;
+
+int currentRainType = 0;
+int currentRainMode = 0;
+uint16_t currentRainColor = 0;
+float rainDuration = 20 * 1000;
+float rainTimer = 0;
+bool inRain = false;
+
+float noRainTimer = 0;
+float noRainDuration = 60;
+
+
+int transitionCycles = 31;
+int transitionCounter = 0;
+int transitionDirection = 1;
+bool inTransition = false;
 
 struct position {
     
@@ -86,7 +107,6 @@ struct position {
 };
 
 QueueArray <position> ghostPositions;
-
 QueueArray<position> rainPositions;
 
 int ghostCounter = 0;
@@ -119,23 +139,13 @@ void simpleBuildBody(int x, int y, uint16_t gc = c){
     matrix.fillCircle(x, y, 10, gc);
 }
 
-void startFade( int iIncrement ){
-    
-    fadeMonster = true;
-    fadeCount = 255;
-    fadeIncrement = iIncrement;
-    
-}
 
-void createRain( int numPoops ){
+void createRain( int numDrops ){
     
-    for (int i = 0; i < numPoops; i++){
-        
-        
+    for (int i = 0; i < numDrops; i++){
         position p = position {
-            
             .x = random(LOW_X, HIGH_X),
-            .y = LOW_Y - random(10,50)
+            .y = LOW_Y - random(10,100)
         };
         
         rainPositions.push( p );
@@ -213,53 +223,106 @@ void drawCloud(int x, int y, int r, uint16_t red, uint16_t green, uint16_t blue)
     
 }
 
-void drawFood(int x, int y){
+void drawCherry(int x, int y){
     
     uint16_t r = matrix.Color333(7,0,0);
     uint16_t g = matrix.Color333(0,7,0);
+    
+    matrix.drawLine(x-2, y-2, x, y-6, g);
+    matrix.drawLine(x+2, y-2, x, y-6, g);
+
+    // leaf
+    matrix.fillTriangle(x,y-6, x+5,y-6,x+3,y-8,g);
+
+    matrix.fillCircle(x - 2, y, 1, r);
+    matrix.fillCircle(x + 2, y, 1, r);
+    
+}
+
+void drawPizza(int x, int y){
+    
+    uint16_t r = matrix.Color333(7,0,0);
     uint16_t yell = matrix.Color333(7,5,0);
     uint16_t o = matrix.Color333(7,3,0);
+    
+    matrix.fillTriangle( x, y, x + 10, y-5, x + 5, y-10, yell);
+    matrix.drawLine(x + 10, y-5, x + 5, y-10,o);
+    matrix.drawLine(x + 10, y-4, x + 4, y-10,o);
+    
+    matrix.drawPixel(x+1,y-1,r);
+    matrix.drawPixel(x+1,y-2,r);
+    matrix.drawPixel(x+2,y-2,r);
+    matrix.drawPixel(x+2,y-3,r);
+    
+    matrix.drawPixel(x+6,y-3,r);
+    matrix.drawPixel(x+5,y-3,r);
+    matrix.drawPixel(x+4,y-2,r);
+    
+    matrix.fillCircle(x + 5, y-6, 1, r);
+}
 
-    // cherry
+void drawHeart(int x, int y, uint16_t color = matrix.Color333(7,0,0)){
+
+    matrix.drawBitmap( x, y, heart, 16, 16, color);
+}
+
+void drawCross(int x, int y, uint16_t color = matrix.Color333(6,3,3)){
+
+    matrix.drawBitmap( x, y, heart, 16, 16, c);
+}
+
+void drawPoop(int x, int y, uint16_t color = matrix.Color333(3,7,3)){
     
-//    matrix.drawLine(x-2, y-2, x, y-6, g);
-//    matrix.drawLine(x+2, y-2, x, y-6, g);
-//    
-//    // leaf
-//    matrix.fillTriangle(x,y-6, x+5,y-6,x+3,y-8,g);
-//    
-//    matrix.fillCircle(x - 2, y, 1, r);
-//    matrix.fillCircle(x + 2, y, 1, r);
+    matrix.drawBitmap( x, y, heart, 16, 16, color);
+}
+
+void drawRain(int x, int y, int type, uint16_t color){
     
-    matrix.drawBitmap( x, y, heart, 16, 16, r);
+    if (currentRainMode == 1){
+        currentRainColor = color;
+    }
     
+    if (type == 0){
+        drawCherry(x,y);
+    }else if (type == 1){
+        drawPizza(x,y);
+    }else if (type == 2){
+        drawHeart(x, y, currentRainColor);
+    }else if (type == 3){
+        drawCross(x, y, currentRainColor);
+    }else if (type == 4){
+        drawPoop(x, y, currentRainColor);
+    }else{
+        drawPizza(x, y);
+    }
+}
+
+uint16_t getMultiColor(int index){
     
-    // pizza
-    
-//    matrix.fillTriangle( x, y, x + 10, y-5, x + 5, y-10, yell);
-//    matrix.drawLine(x + 10, y-5, x + 5, y-10,o);
-//    matrix.drawLine(x + 10, y-4, x + 4, y-10,o);
-//    
-//    matrix.drawPixel(x+1,y-1,r);
-//    matrix.drawPixel(x+1,y-2,r);
-//    matrix.drawPixel(x+2,y-2,r);
-//    matrix.drawPixel(x+2,y-3,r);
-//    
-//    matrix.drawPixel(x+6,y-3,r);
-//    matrix.drawPixel(x+5,y-3,r);
-//    matrix.drawPixel(x+4,y-2,r);
-    
-//    matrix.fillCircle(x + 5, y-6, 1, r);
-    
-    
-    
-    
-    
+    // num colors =
+    // pink, blue, green, white
+    if (index % 4 == 0 ){
+        
+        return matrix.Color333(7,7,7);
+        
+    }else if (index % 4 == 0 ){
+        
+        return matrix.Color333(7,4,4);
+        
+    }else if (index % 4 == 0 ){
+
+        return matrix.Color333(4,7,4);
+        
+    }else{
+        
+        return matrix.Color333(4,4,7);
+        
+    }
 }
 
 
 
-void updateRain(){
+void updateRain(int type){
     
     int numRain = rainPositions.count();
     
@@ -271,21 +334,19 @@ void updateRain(){
         
         position thisPos = rainPositions.pop();
         
-        
-//        matrix.drawBitmap(thisPos.x, thisPos.y,cross,8,8,p);
-        
-        drawFood(thisPos.x, thisPos.y);
-        // draw thing
-        
+        drawRain(thisPos.x, thisPos.y, type, getMultiColor(ind));
+
         position newPos = {
             
             .x = thisPos.x,
             .y = thisPos.y + 2
         };
         
-        if (newPos.y > HIGH_Y){
+        if (newPos.y > (HIGH_Y + 10)){
             
-            newPos.y = LOW_Y - random(10,20);
+            if (inRain){
+                newPos.y = LOW_Y - random(10,20);
+            }
         }
         
         // add back on
@@ -297,60 +358,6 @@ void updateRain(){
 }
 
 
-
-void buildBody(int x, int y, uint16_t gc = c){
-    
-//    int x = 32;
-//    int y = 16;
-    
-    
-    // fill a violet circle
-    matrix.fillCircle(x, y, 10, gc);
-    
-    
-    // build eyes
-    // 2 4 pix rectangles
-    int ax = -4;
-    int ay = -3;
-    
-    // right eye
-    matrix.fillRect(x + 4, y + ay, 3,3, b);
-    matrix.fillRect(x + 4, y-1 + ay, 2,3, b);
-    
-    // make other eye
-    matrix.fillRect(x -4 -2, y + ay, 3,3, b);
-    matrix.fillRect(x -4 -2, y-1 + ay, 2,3, b);
-    
-    
-    // + y is down.
-    
-    // make a mouth
-    matrix.fillRect(x - 3, y + 4, 7,2,b);
-    
-}
-
-void buildEvilBody(){
-    
-    int x = 32;
-    int y = 16;
-    
-    // draw body
-//    matrix.drawBitmap(x,y, m_body, 16, 8, p) ;
-    
-    // draw head
-//    matrix.drawBitmap(20,y - 10, m_head, 16, 16, g) ;
-    
-//    matrix.drawBitmap(30,y-10, m_w_body, 32, 16, g);
-    
-    // draw a poop
-    matrix.drawBitmap(10,10,poop,16,16,c);
-    
-    // make some noise
-    
-
-    
-    
-}
 
 void drawGhosts(void (*f)(int,int,uint16_t) ){
     
@@ -379,71 +386,223 @@ void drawGhosts(void (*f)(int,int,uint16_t) ){
     }
 }
 
+
+void selectRainColor(){
+    
+    currentRainMode = 0; // use color
+    
+    int type = currentRainType;
+    
+    // Cherry, don't change
+    if (type == 2){
+
+        // pick random number
+        int _rand = random(0,11);
+        
+        // red
+        if (_rand <= 3){
+            
+            currentRainColor = matrix.Color333(7, 0, 0);
+         
+        // pink
+        }else if (_rand <= 6){
+            
+            currentRainColor = matrix.Color333(7, 4, 4);
+            
+        // multi
+        }else{
+            
+            currentRainMode = 1;
+        }
+        
+    // CROSS
+    }else if (type == 3){
+        
+        // pick random number
+        int _rand = random(0,11);
+        
+        // pink
+        if (_rand <= 2){
+            
+            currentRainColor = matrix.Color333(7, 4, 4);
+            
+        // blue
+        }else if (_rand <= 5){
+            
+            currentRainColor = matrix.Color333(4, 4, 7);
+            
+        // white
+        }else if (_rand <= 7){
+            
+            currentRainColor = matrix.Color333(7,7,7);
+        }else{
+            currentRainMode = 1;
+        }
+        
+        
+    // poopy
+    }else if (type == 4){
+        
+        // green red
+        currentRainColor = matrix.Color333(3,7,3);
+
+    }
+}
+
+
+
+void startRain(){
+    
+    
+    noRainTimer = 0;
+    
+    rainDuration = (float) random(5,20) * 1000;
+    rainTimer = 0;
+    inRain = true;
+    currentRainType = (int) floor(random(0,5));
+    selectRainColor();
+    
+    
+    Serial.println("rain: " + currentRainType);
+    
+}
+
 void setup() {
     
+    currentRainColor = matrix.Color333(7,7,7);
     
     createRain(10);
 
     Serial.begin(9600);
     Serial.println("Hello world");
     
-matrix.begin();
+    matrix.begin();
 
-// fill the screen with 'black'
-matrix.fillScreen(matrix.Color333(0, 0, 0));
-delay(500);
+    // fill the screen with 'black'
+    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    delay(500);
 
     Serial.begin(9600);
     Serial.println("hello color");
 
-//    buildBody();
+    // Choose random duration
     
-//    buildEvilBody();
-
-//    startFade(1);
-
+    startRain();
+    
 
 }
 
 
-void drawGradient(){
+
+void stopRain(){
     
-    for (int i = 0; i < 32; i++){
+    inRain = false;
+    rainTimer = 0;
+    noRainTimer = 0;
+    noRainDuration = 1000 * random(30,60);
+    
+    
+}
+
+void changeGhost(){
+    
+    ghostTypeTimer = 0;
+    ghostTypeInterval = random(60,90) * 1000;
+    
+    // Change current monster
+    currentGhostType = (int) floor(random(0,4));
+    
+}
+
+
+void startTransition(){
+    
+    inTransition = true;
+    transitionDirection = 1;
+
+}
+
+void stopTransition(){
+    
+    
+    inTransition = false;
+    transitionCounter = 0;
+    transitionDirection = 1;
+    
+}
+
+void updateTransition(){
+    
+    if ( transitionCounter > transitionCycles){
         
-        int sR = 255;
-        int sG = 200;
-        int sB = 50;
+        transitionCounter = transitionCycles;
+        transitionDirection = -1;
         
-        int eR = 50;
-        int eG = 0;
-        int eB =150;
+        changeGhost();
         
+        // Draw sunset with counter number of lines
+        drawGradient( transitionCounter + 1);
+
         
-        // get normalized val
-        float normVal = i / 32.0f;
+    }else if (transitionCounter < 0){
         
-        int nR = (1.0 - normVal) * sR + normVal * eR;
-        int nG = (1.0 - normVal) * sG + normVal * eG;
-        int nB = (1.0 - normVal) * sB + normVal * eB;
+        // DONE, change monster
+        stopTransition();
         
-        matrix.drawFastHLine(0, i, 64, matrix.Color888( nR, nG, nB));
-        
+    }else{
+    
+        // Draw sunset with counter number of lines
+        drawGradient( transitionCounter + 1);
         
     }
 
-    
 }
+
+void updateTimers(){
+    
+    // Calculate delta time for this frame
+    unsigned long curMillis = millis();
+    float dt = curMillis - prevMillis;
+    
+    // Update each timer
+    ghostTypeTimer += dt;
+    
+    if (ghostTypeTimer >= ghostTypeInterval){
+        startTransition();
+    }
+    
+    if (inTransition){
+        transitionCounter++;
+        updateTransition();
+    }
+    
+    if (inRain){
+        rainTimer += dt;
+        
+        if (rainTimer >= rainDuration){
+            stopRain();
+        }
+    }else{
+        
+        noRainTimer += dt;
+        
+        if (noRainTimer >= noRainDuration){
+            
+            startRain();
+        }
+    }
+    
+    
+    
+    // At end
+    prevMillis = curMillis;
+}
+
+
 
 void drawMonster(int type, int red, int green, int blue){
     
-    float fade = fadeCount / 255.0f;
-    
-    if (fadeMonster){
-        
-        
-    }
-    
-    uint16_t val = matrix.Color888(red * fadeCount, green* fadeCount, blue* fadeCount);
+    uint16_t val = matrix.Color888(red, green, blue);
     
     if (type == 0){
         
@@ -464,7 +623,7 @@ void drawMonster(int type, int red, int green, int blue){
         matrix.drawFastHLine(curX + 5, 30, 15, val);
 
         
-    }else{
+    }else if (type == 2){
         
         if (x_sign < 0){
             
@@ -477,12 +636,14 @@ void drawMonster(int type, int red, int green, int blue){
             matrix.drawFastHLine(curX, 30, 20, val);
         }
         
+    }else{
+        drawCloud(20,0,30, 200.0, 150.0f,255.0f);
     }
 }
 
 void loop() {
     
-    matrix.fillScreen(matrix.Color333(0, 0, 0));
+    matrix.fillScreen(matrix.Color333(0,0,0));
     
     // Perlin test.
     
@@ -503,8 +664,6 @@ void loop() {
     long remapY = map(yn, 0.0, 255.0, 2.0, 4.0);
     
     // Increment position
-    
-    
     position p = {
         .x = curX,
         .y = curY
@@ -516,26 +675,17 @@ void loop() {
         ghostPositions.pop();
     }
 
-    
-//    drawGradient();
-//    drawCloud(20,0,30, 200.0, 150.0f,255.0f);
-//    updateRain();
-//    drawFood(20,20);
-    
-//    drawGhosts( simpleBuildBody );
-    
-    drawMonster(2, 255, 255, 255);
+    updateTimers();
+
+    updateRain( currentRainType );
+
+    drawMonster(currentGhostType, 255, 255, 255);
     
     float bobVar = sin( radians( curTime ) * 0.3f ) * 2.0;
     
     curY = bobVar;
-    
-//    
     curX += x_sign * remapX;
-//    curY += y_sign * remapY;
-    
-//    Serial.println(curX);
-//    Serial.println(curY);
+
     
     // Check if out of bounds, if so, flip sign
     if (curX <= LOW_X){
@@ -556,30 +706,9 @@ void loop() {
         
         y_sign = -1;
     }
-    
-    
-    if (fadeMonster){
-        
-        fadeCount-=fadeIncrement;
-        
-        if (fadeCount <=0 ){
-            
-            fadeMonster = false;
-            
-        }
-    }
-    
-    
-    // Clear screen
 
-    
     matrix.swapBuffers(true);
-//    delay(0);
-    
-    ghostCounter++;
-    
-    
-// do nothing
+
 }
 
 
